d0899f5a1c276442ce35f3372b86890b5b1f4333
алина человечкова <alinasmart19@mail.ru>
2021-02-01 19:28:45 +0600

    Добавьте файлы проекта.

---
+++ ButtonClass/Button.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+namespace ButtonClass
+{
+    public delegate void ButtonActionDelegate(params object[] array);
+
+    public class Button
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Text { get; set; }
+        public string Color { get; set; }
+        public ConsoleColor hBorderColor;
+
+        public ButtonActionDelegate ButtonAction { get; set; }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public string Draw()
+        {
+            string s = "╔";
+            string space = "";
+            string temp = "";
+            for (int i = 0; i < Width; i++)
+            {
+                space += " ";
+                s += "═";
+            }
+            s += "╗" + "\n";
+            string space2 = space;
+            for (int i = 0; i < Height; i++)
+            {
+                if (i == 1)
+                {
+                    s += temp + "║" + Text +space2.Remove(0,Text.Length) + "║" + "\n";
+                }
+                else
+                    s += temp + "║" + space + "║" + "\n";
+            }
+
+            s += temp + "╚";
+            for (int i = 0; i < Width; i++)
+                s += "═";
+
+            s += "╝" + "\n";
+
+            Console.ForegroundColor = hBorderColor;
+            return s;
+        }
+    }
+}
---
+++ ButtonClass/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ButtonClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var button = new Button
+            {
+                Width = 9,
+                Height = 3,
+                Text = "Click",
+                hBorderColor = ConsoleColor.Red,
+                ButtonAction = new ButtonActionDelegate(Console.WriteLine)
+            };
+
+            button.ButtonAction += new ButtonActionDelegate(Save);
+            Console.WriteLine(button.Draw());
+        }
+
+        private static void Save(object data)
+        {
+
+        }
+
+    }
+}
